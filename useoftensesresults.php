5b8c9a4ef74d13e0b619cb46158833c74dfec0c6
WebsiteBuilder7867 <37977300+WebsiteBuilder7867@users.noreply.github.com>
2020-02-17 20:32:50 +0000

    Add files via upload

---
+++ useoftensesresults.php
@@ -0,0 +1,378 @@
+
+<!DOCTYPE html>
+<html lang="en-GB">
+  <head>
+    <meta charset="UTF-8">
+    <title>Answers for Use of Tenses Quiz</title>
+    <meta name="description" content="Results of useoftenses quiz">
+    <meta name="keywords" content="use of tenses, ielts">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="style.css" rel="stylesheet"><!--must come after bootstrap-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+    
+        <div class="row"><!-- row 1 -->        
+          <div class="col-sm-12">
+            <h1>Use of Tenses - Results</h1>
+          </div>     
+        </div><!-- end row 1-->    
+    
+            <?php
+              $question1 = $_POST['question1'];
+              $question2 = $_POST['question2'];
+              $question3 = $_POST['question3'];
+              $question4 = $_POST['question4'];
+              $question5 = $_POST['question5'];
+              $question6 = $_POST['question6'];
+              $question7 = $_POST['question7'];
+              $question8 = $_POST['question8'];
+              $question9 = $_POST['question9'];
+              $question10 = $_POST['question10'];
+              $question11 = $_POST['question11'];
+              $question12 = $_POST['question12'];
+              $question13 = $_POST['question13'];
+              $question14 = $_POST['question14'];
+              $question15 = $_POST['question15'];
+              $question16 = $_POST['question16'];
+              $question17 = $_POST['question17'];
+              $question18 = $_POST['question18'];
+              $question19 = $_POST['question19'];
+              $question20 = $_POST['question20'];
+            ?>
+
+            <br>
+
+        <div class="row"><!--row 1-->
+          <div class="col-sm-3"><!--col 1-->            
+            <div ="centeralign">
+              <p>1.&nbsp; &nbsp; 
+                <?php
+                  if ($question1 == "answer1.1")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>2.&nbsp; &nbsp;
+                <?php
+                  if ($question2 == "answer2.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>3.&nbsp; &nbsp;
+                <?php
+                  if ($question3 == "answer3.1")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>4.&nbsp; &nbsp;
+                <?php
+                  if ($question4 == "answer4.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>5.&nbsp; &nbsp;
+                <?php
+                  if ($question5 == "answer5.3")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                    echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+            </div><!--col 1--> 
+
+            <div class="col-sm-3"><!-- col 2 -->
+              <p>6.&nbsp; &nbsp;
+                <?php
+                  if ($question6 == "answer6.2")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+            <div><!--col 1-->
+                
+            <div><!--col 2-->
+              <p>7.&nbsp; &nbsp;
+                <?php
+                  if ($question7 == "answer7.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>8.&nbsp; &nbsp;
+                <?php
+                  if ($question8 == "answer8.2")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>9.&nbsp; &nbsp;
+                <?php
+                  if ($question9 == "answer9.1")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>10.&nbsp;
+                <?php 
+                  if ($question10 == "answer10.2")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+            </div><!--end col 2-->
+                              
+            <div class="col-sm-3"><!-- col 3 -->
+              <p>11.&nbsp;
+                <?php
+                  if ($question11 == "answer11.3")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>12.&nbsp;
+                <?php
+                  if ($question12 == "answer12.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>13.&nbsp; 
+                <?php
+                  if ($question13 == "answer13.3")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>14.&nbsp;
+                <?php
+                  if ($question14 == "answer14.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>15.&nbsp;
+                <?php
+                  if ($question15 == "answer15.1")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+            </div><!--end col 3-->
+
+            <div class="col-sm-3"><!-- col 4 -->
+              <p>16.&nbsp;
+                <?php
+                  if ($question16 == "answer16.2")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>17.&nbsp;
+                <?php
+                  if ($question17 == "answer17.4")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+                </p>
+
+              <p>18.&nbsp;
+                <?php
+                  if ($question18 == "answer18.1")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>19.&nbsp; 
+                <?php
+                  if ($question19 == "answer19.3")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+
+              <p>20.&nbsp;
+                <?php
+                  if ($question20 == "answer20.2")
+                  {
+                  echo ("&nbsp; &nbsp;Correct<br>");
+                  }
+                  else
+                  {
+                  echo ("<span style='color:red;'>Incorrect</span><br>");
+                  }
+                ?>
+              </p>
+            </div><!--col 4-->
+          </div><!--row 1--> 
+
+                <?php
+                  $totalCorrect = 0;
+
+                  if ($question1 == "answer1.1") { $totalCorrect++; }
+                  if ($question2 == "answer2.4") { $totalCorrect++; }
+                  if ($question3 == "answer3.1") { $totalCorrect++; }
+                  if ($question4 == "answer4.4") { $totalCorrect++; }
+                  if ($question5 == "answer5.3") { $totalCorrect++; }
+                  if ($question6 == "answer6.2") { $totalCorrect++; }
+                  if ($question7 == "answer7.4") { $totalCorrect++; }
+                  if ($question8 == "answer8.2") { $totalCorrect++; }
+                  if ($question9 == "answer9.1") { $totalCorrect++; }
+                  if ($question10 == "answer10.2") { $totalCorrect++; }
+                  if ($question11 == "answer11.3") { $totalCorrect++; }
+                  if ($question12 == "answer12.4") { $totalCorrect++; }
+                  if ($question14 == "answer14.4") { $totalCorrect++; }
+                  if ($question15 == "answer15.1") { $totalCorrect++; }
+                  if ($question16 == "answer16.2") { $totalCorrect++; }
+                  if ($question17 == "answer17.4") { $totalCorrect++; }
+                  if ($question18 == "answer18.1") { $totalCorrect++; }
+                  if ($question19 == "answer19.3") { $totalCorrect++; }
+                  if ($question20 == "answer20.2") { $totalCorrect++; }
+                ?>
+
+          <div class="row"><!-- row2 -->
+            <div class="col-sm-3"><!--col 1-->
+              <?php 
+                echo ("<p style='text-align:center;'><a href='index.html'>Home Page</a></p>");
+              ?>
+            </div><!--col 1-->
+
+            <div class="col-sm-3"><!--col 2-->
+              <?php 
+                echo ("<p style='text-align:center;'><a href='useoftenses.html'>Try again</a></p>");
+              ?>
+            </div><!--col 2-->
+          </div><!-- row2-->
+
+
+          <div class="row"><!--row3-->
+            <div class="col-sm-12"><!--col 1-->
+              <?php 
+                echo ("<div id='results'><p style='text-align:center;color:#2d862d;'>Score: $totalCorrect / 20 </p></div>");
+              ?>
+            </div><!--col 1-->
+          </div><!--row3-->
+
+        </div><!--centeralign-->
+
+        <div class="copyright">    
+          <p>Copyright 2017 by Steven WJ Richards.<br>All Rights Reserved.</p>
+        </div><!--copyright-->
+
+      </div><!--content-->
+    </div><!--container-->
+  </body>
+</html>
